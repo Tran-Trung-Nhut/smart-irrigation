39e4f0a117fd233051aeaaa1ab7ccf786373a6e4
Lâm Bảo Minh <lambaominh2016@gmail.com>
2025-03-23 21:28:42 +0700

    fix Degree.java + Lux.java

--- backend/src/main/java/com/smartirrigation/smart_irrigation/models/SensorData.java
+++ backend/src/main/java/com/smartirrigation/smart_irrigation/models/SensorData.java
@@ -1,15 +1,15 @@
 package com.smartirrigation.smart_irrigation.models;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Degree {
+public class SensorData {
     private String id;
     private Double value;
     private int feed_id;
@@ -17,4 +17,5 @@ public class Degree {
     private String created_at;
     private long created_epoch;
     private String expiration;
+    private String type; 
 }
